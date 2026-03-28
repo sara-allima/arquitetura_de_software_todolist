b25725705a39eb87137b919b085f57a6b81301ad
Sara Lima <saramalima82@gmail.com>
2026-03-28 18:33:03 -0300

    Tailwind configurado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1 className='text-amber-300 font-medium'>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
