7fa005bb019886460f6706ea2a7d20890f585af5
Sara Lima <saramalima82@gmail.com>
2026-03-29 11:07:50 -0300

    Delete task

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -14,8 +14,16 @@ export default function TaskCard({ task }) {
       {...attributes}
       style={style}
       className="bg-slate-600 p-4 rounded-xl shadow-md mb-4 cursor-grab">
-      <h3 className="text-white font-semibold">{task.title}</h3>
-      <p className="text-gray-300 text-sm">{task.description}</p>
+      <div className="flex justify-between items-start">
+        <div>
+          <h3 className="text-white font-semibold">{task.title}</h3>
+          <p className="text-gray-300 text-sm">{task.description}</p>
+        </div>
+
+        <div className="flex gap-2 text-red-400 cursor-pointer">
+          <span>🗑</span>
+        </div>
+      </div>
     </div>
   );
 }
